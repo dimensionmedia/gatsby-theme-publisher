3c055c3e1bcb68fc0066a87b123137e4b5bfb44f
David Bisset <dbisset@dimensionmedia.com>
2019-08-18 13:30:54 -0400

    Updating MailChimp Endpoint

--- config.js
+++ config.js
@@ -15,7 +15,7 @@ const config = {
   siteUrl: `https://practical-noyce-13759b.netlify.com`,
   dynamicComments: true,
   gaTrackingId: '',
-  mailChimpEndpoint: 'https://rocks.us3.list-manage.com/subscribe/post?u=9489c859df521aeb6956b08ed&amp;id=0b6862abcd',
+  mailChimpEndpoint: 'https://rocks.us3.list-manage.com/subscribe/post?u=9489c859df521aeb6956b08ed',
 }
 
 module.exports = config
